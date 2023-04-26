5f5daba861c4ff7f2405d1e873d760446c40c0e7
yamamoto yuki <yamamotoyuki.bungu.pc@gmail.com>
2023-04-26 16:23:46 +0900

    selenium_wait.pyまで完了

--- python/selenium_wait.py
+++ python/selenium_wait.py
@@ -4,14 +4,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
-url = "file:///C:/Users/writer/study_work/html/wait.html"
+import sys
+sys.path.append("..")
+from path.path import *
+
+url = HTML_FILE_PATH + "wait.html"
+
 driver = webdriver.Chrome()
 driver.get(url)
+
 # 要素(id='msg')が現れるまで待機
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "msg")))
-print(driver.find_element_by_id("msg").text)
+print(driver.find_element(By.ID, "msg").text)
+
 # 要素(id='btn')がクリック可能まで待機
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btn")))
-print(driver.find_element_by_id("btn").get_attribute("value"))
+print(driver.find_element(By.ID, "btn").get_attribute("value"))
 time.sleep(3)
 driver.quit()
